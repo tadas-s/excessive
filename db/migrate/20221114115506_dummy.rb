1a084ae98c0ee687a3d342f310f63f28bb7c0d27
Tadas Sasnauskas <tadas@yoyo.lt>
2022-11-14 14:08:07 +0200

    Add dummy migration/model
    For playing with migrations during deployments.

---
+++ db/migrate/20221114115506_dummy.rb
@@ -0,0 +1,5 @@
+class Dummy < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dummies
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_115506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dummies", force: :cascade do |t|
+  end
 
 end
